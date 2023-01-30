a303b8adb833388ccb877c93d7ead32719fa23eb
hfeild <hfeild@host.docker.internal>
2023-01-30 05:56:07 -0500

    Added virutalpet project

---
+++ virtualpet/src/main/java/edu/endicott/csc/virtualpet/App.java
@@ -0,0 +1,13 @@
+package edu.endicott.csc.virtualpet;
+
+
+/**
+ *
+ * @author Michael Ocean <mocean@endicott.edu>
+ */
+public class App {
+    public static void main(String[] args){
+        MainWindow mw = new MainWindow();
+        mw.setVisible(true);
+    }
+}
